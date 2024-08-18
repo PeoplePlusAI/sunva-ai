d2e8088d44ea08a25072a5dcb6a65e9a9fe8531b
Gopi (GKS) <gopikrishnans1996@gmail.com>
2024-08-18 08:25:28 +0530

    Added db support to stt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from core.db.init_db import init_db
 from fastapi.responses import FileResponse
 from core.api import stt, tts
 
@@ -10,6 +11,10 @@ app.include_router(tts.router)
 @app.get("/")
 async def get():
     return FileResponse("static/index.html")
+
+@app.on_event("startup")
+async def on_startup():
+    await init_db()
 
 
 if __name__ == "__main__":
