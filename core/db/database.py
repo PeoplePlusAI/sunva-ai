0882f5b0bf4dfbf8114d3877bf7226ce2ea42485
BSB <bharat.shetty@gmail.com>
2024-09-25 11:16:00 +0530

    Fix the async prepared statement in sqlalchemy (#69)

--- core/db/database.py
+++ core/db/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
+import sqlalchemy as sa
+
 from dotenv import load_dotenv
 import os
 load_dotenv(
@@ -10,8 +12,12 @@ load_dotenv(
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create the async engine
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
-
+engine = create_async_engine(DATABASE_URL, echo=True, future=True, poolclass=sa.NullPool,
+                             connect_args={
+                                 "prepared_statement_cache_size": 0,
+                                 "statement_cache_size": 0
+                                 }
+                             )
 # Create the async session factory
 async_session = sessionmaker(
     bind=engine,
